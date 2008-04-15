71568f1c8435a159e5edc9f137ed2c6f8ee10503
dcparker <dcparker@0eb1a872-461d-0410-a379-984e9e9c21cf>
2008-04-15 16:44:00 +0000

    Removed bulky test company file.
    git-svn-id: http://quickbooks.rubyforge.org/svn@24 0eb1a872-461d-0410-a379-984e9e9c21cf

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -1,3 +1,7 @@
+# In order to run these tests you need to have a test Company file. I didn't include one because it would take up the bulk of the size
+# of an svn checkout. Just create a new company and open it. Make a couple people, and change some of the tests to include the right
+# names or whatever they reference. Sorry, it's not necessarily meant to be easy.
+
 $TESTING=true
 $:.push File.join(File.dirname(__FILE__), '..', 'lib')
 require 'quickbooks'
