46ee32dfed082f17a1ae5519d56d2e2943a3c121
Daniel Parker <dcparker@gmail.com>
2008-08-05 18:45:49 -0400

    Enhanced testing.

--- lib/quickbooks/adapters/test_adapter.rb
+++ lib/quickbooks/adapters/test_adapter.rb
@@ -5,6 +5,11 @@ module Quickbooks
       def initialize(*args)
       end
 
+      def next_response(value, &block)
+        @next_value = value
+        @next_block = block
+      end
+
       # Returns true if there is an open connection to Quickbooks, false if not. Use session? to determine an open session.
       def connected?
         @connected ||= false
@@ -12,7 +17,9 @@ module Quickbooks
 
       # Sends a request to Quickbooks. This request should be a valid QBXML request. Use Qbxml::Request to generate valid requests.
       def send_xml(xml)
-        return xml.to_s
+        @next_block.call(xml) if @next_block
+        raise RuntimeError, "You must call next_response to set up the next response xml manually for the TestAdapter." unless @next_value
+        return @next_value
       end
 
       # Returns the active connection to Quickbooks, or creates a new one if none exists.
